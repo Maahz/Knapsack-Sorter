c654300a08c1af1be8a81db17d6cacb3ea6aae14
Markku Sjöblom <markku.sjoblom@gmail.com>
2021-05-15 23:17:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Knapsack_Sorter
+{
+    public class Knapsack
+    {
+        public int Size;
+    }
+    
+    public class Item
+    {
+        public string Name;
+        public int Value;
+        public int Weight;
+        public double Ratio ;
+    }
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Knapsack shitHole = new Knapsack();
+            shitHole.Size = 20;
+        }
+    }
+}
